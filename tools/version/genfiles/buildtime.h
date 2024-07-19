21a4e6020b8eb81d42fe33949501ca148add1622
Taylor Carpenter <taylor@carpen.tech>
2024-07-19 12:25:24 -0400

    Clean copy of sdk v3.2.8.1

---
+++ tools/version/genfiles/buildtime.h
@@ -0,0 +1,45 @@
+/**
+ ****************************************************************************************
+ *
+ * @buildtime.h
+ *
+ * @brief Build Time Generation
+ *
+ * Copyright (c) 2016-2022 Renesas Electronics. All rights reserved.
+ *
+ * This software ("Software") is owned by Renesas Electronics.
+ *
+ * By using this Software you agree that Renesas Electronics retains all
+ * intellectual property and proprietary rights in and to this Software and any
+ * use, reproduction, disclosure or distribution of the Software without express
+ * written permission or a license agreement from Renesas Electronics is
+ * strictly prohibited. This Software is solely for use on or in conjunction
+ * with Renesas Electronics products.
+ *
+ * EXCEPT AS OTHERWISE PROVIDED IN A LICENSE AGREEMENT BETWEEN THE PARTIES, THE
+ * SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. EXCEPT AS OTHERWISE
+ * PROVIDED IN A LICENSE AGREEMENT BETWEEN THE PARTIES, IN NO EVENT SHALL
+ * RENESAS ELECTRONICS BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT, INCIDENTAL,
+ * OR CONSEQUENTIAL DAMAGES, OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF
+ * USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+ * TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE
+ * OF THE SOFTWARE.
+ *
+ ****************************************************************************************
+ */
+
+#ifndef __buildtime_h__
+#define __buildtime_h__
+
+#define REV_BUILDFULLTIME			202311071043
+#define REV_BUILDDATE				20231107
+#define REV_BUILDTIME				104329
+#define REV_BUILDDATE_HEX			0x20231107
+#define REV_BUILDTIME_HEX			0x00104329
+#define SVN_REPOSITORY				"iotwifi_da16200/da16200_freertos.git branch DA16200_3_2_master"
+#define SVN_REVISION_NUMBER		2aa77df370
+#define SVN_REVISION_NUMBER_HEX	0x2aa77df370
+
+#endif /*__buildtime_h__*/
